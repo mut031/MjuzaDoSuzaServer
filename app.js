a63b89d526369a95ec92f384b237f8b7a83aeee0
mut031 <f.mutnjakovic@gmail.com>
2019-09-25 00:50:52 +0200

    DeprecationWarning

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const scraper = require('./scraper')
 
-const port = 3002;
+const port = 3000;
 
 app.use(express.static('www'));
 
@@ -19,47 +19,22 @@ database.then(res => {
     db = res;
     Promise.all(setupDb(res)).then(() => {
         //http instead of app because of socket.io
-        http.listen(port, () => console.log('Listening on port 3002'));
+        http.listen(port, () => console.log('Listening on port 3000'));
     })
 });
 
 //add song to playlist
 app.post('/playlist', (req, res) => {
-    db.collection('playlist').find({ $or: [{ id: req.body.item.id }, { isCurrent: true }] }).toArray((err, result) => {
-        if (err) throw err;
-        let response = {};
-        switch (result.length) {
-            case 0:
-                db.collection('playlist').insertOne({ isCurrent: true, ...req.body.item });
-                response = {
-                    message: "Song added to playlist!",
-                    status: 'success'
-                }
-                break;
-            case 1:
-                if (req.body.item.id != result[0].id) {
-                    db.collection('playlist').insertOne({ isCurrent: false, ...req.body.item });
-                    response = {
-                        message: "Song added to playlist!",
-                        status: 'success'
-                    }
-                }
-                else
-                    response = {
-                        message: "This song is playing right now!",
-                        status: 'primary'
-                    }
-                break;
-            default:
-                response = {
-                    message: "This song is already on the playlist!",
-                    status: 'danger'
-                }
-                break;
-        }
-
-        res.send(response);
-    });
+    db.collection('playlist').countDocuments()
+        .then((count) => {
+            db.collection('playlist').insertOne({ _id: req.body.item.id, isCurrent: count === 0, ...req.body.item })
+                .then(() => {
+                    res.send({ message: "Song added to playlist!", status: 'primary' });
+                })
+                .catch(() => {
+                    res.send({ message: "This song is already on the playlist!", status: 'danger' });
+                });
+        });
 });
 
 //get songs in playlist
@@ -67,7 +42,6 @@ app.get('/playlist', (req, res) => {
     db.collection('playlist').find().toArray((err, result) => {
         if (err) throw err;
         //filter result nađi videoId
-
         res.send(result);
     });
 });
@@ -76,21 +50,39 @@ app.get('/playlist', (req, res) => {
 app.delete('/playlist', (req, res) => {
     if (!req.body.isCurrent)
         db.collection('playlist').deleteOne({ 'id': req.body.id })
-            .then(data => {
+            .then(() => {
                 io.emit('update');
-                res.send(data);
+                res.send({message: "Song deleted from playlist!", status: 'primary'});
             });
-    else
-        res.send({ mrkiljakse: 'najveci' })
+    else 
+        res.send({message: "Cannot delete this song!", status: 'danger'});
 });
 
 //update current song
 app.put('/playlist', (req, res) => {
+    // // db.collection('playlist').updateMany(
+    // //     { _id: { $in: [req.body.newId, req.body.currentId]}},
+    // //     { $set: { "isCurrent" : $isCurrent }}
+    // // )
+    
+    // db.collection('playlist').find({ _id: { $in: [req.body.newId, req.body.currentId]}}).snapshot().forEach(elem => {
+    //         db.collection('playlist').updateOne(
+    //             { _id: elem._id },
+    //             { $set: { isCurrent: !elem.isCurrent } }
+    //         );
+    //     }
+    // )
+    // .then(() => {
+    //     io.emit('update');
+    //     res.send();
+    // })
     var myquery = { id: req.body.id };
     var newvalues = { $set: { isCurrent: req.body.isNew } };
     db.collection('playlist').updateOne(myquery, newvalues, function (err, res) {
         if (err) throw err;
         console.log("1 document updated");
+        //2 puta get na mijenjanje pjesme
+        io.emit('update');
     });
     res.send({ mrki: 'ljakse' });
 });
--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mongoClient = require('mongodb').MongoClient;
 let db = new Promise((resolve, reject) => {
-    mongoClient.connect('mongodb://majkprpik:ibrica98@ds233541.mlab.com:33541/mjuzadosuza', (err, client) => {
+    mongoClient.connect('mongodb://majkprpik:ibrica98@ds233541.mlab.com:33541/mjuzadosuza', { useNewUrlParser: true }, (err, client) => {
         if (err) throw err;
         resolve(client.db('mjuzadosuza'));
     });
